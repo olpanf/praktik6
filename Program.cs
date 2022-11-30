7f0d78f15206beb4a7be6742f63a875013d3a053
olpanf <113999331+olpanf@users.noreply.github.com>
2022-11-30 20:28:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static List<MyDataFile> DataList = new List<MyDataFile>() {
+            new MyDataFile("Рыба 1кг", 1000),
+            new MyDataFile("Курица 1кг", 120),
+            new MyDataFile("Креветки 1кг", 250),
+            new MyDataFile("Утка 1кг", 200)
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("├ Введите файл который хотите открыть (.txt, .json, .xml)");
+
+            Console.Write($"\n{Filear.url} > ");
+            string input = Console.ReadLine();
+            PathToFile(input);
+        }
+
+        public static void PathToFile(string readLine)
+        {
+            Filear.dir = readLine;
+
+            if (File.Exists(Filear.url + "/" + Filear.dir))
+            {
+                Console.Clear();
+                Filear.Open();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Файл: {Filear.dir} - не найден!");
+
+                if (Filear.Create())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Создаю новый файл: {Filear.url}/{Filear.dir}");
+                }
+                else Console.WriteLine($"Файл: {Filear.dir} - не удалось создать");
+
+                Close();
+            }
+        }
+
+        public static void Close()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.White;
+            Environment.Exit(0);
+        }
+    }
+}
